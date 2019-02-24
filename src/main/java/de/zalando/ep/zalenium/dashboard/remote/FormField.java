a1c6e2adee9700e3a82de3cea0b6ade3b41481b3
prashanth-sams <sams.prashanth@gmail.com>
2019-02-24 18:50:46 +0400

    remove files

--- src/main/java/de/zalando/ep/zalenium/dashboard/remote/FormField.java
+++
@@ -1,5 +0,0 @@
-package de.zalando.ep.zalenium.dashboard.remote;
-
-public class FormField {
-    public String keyName;
-}
--- src/main/java/de/zalando/ep/zalenium/dashboard/remote/FormKeyValuePair.java
+++
@@ -1,8 +0,0 @@
-package de.zalando.ep.zalenium.dashboard.remote;
-
-import org.apache.http.entity.ContentType;
-
-public class FormKeyValuePair extends FormField {
-    public String value;
-    public ContentType mimeType;
-}
